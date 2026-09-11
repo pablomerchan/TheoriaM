8eefbe7bd9f1a0cccd8dcb651febbf4b97aaf8ab
Pablo Merchan <pablomerchan@gmail.com>
2026-09-11 10:35:54 -0500

    Doc: Cambios en la documentaacion y la forma de iniciar automaticamente el proyecto.

---
+++ iniciar.bat
@@ -0,0 +1,8 @@
+cd C:\Empresas\TheoriaM\src\backend\
+python C:\Empresas\TheoriaM\src\backend\backend.py
+
+
+cd C:\Empresas\TheoriaM\src\frontend\
+npm start
+
+
---
+++ start.ps1
@@ -0,0 +1,2 @@
+Start-Process powershell -ArgumentList '-NoExit cd C:\Empresas\TheoriaM\src\backend; python backend.py'
+Start-Process powershell -ArgumentList '-NoExit cd C:\Empresas\TheoriaM\src\frontend; npm start'
